3c46bb5356905ee990fe134158e8bb94976b3cec
Ivan Prikhodko <ivanp93@gmail.com>
2023-06-25 20:22:04 -0400

    Set up Mongo

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
+
+const mongoose = require('mongoose');
+
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://ivan:ivanpassword@comp229cluster.5wsntz4.mongodb.net/books229?retryWrites=true&w=majority"
 };
